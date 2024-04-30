e1fb1e44487fb286555abc3eae674add19f93fb3
Simon Dräger <xvertexsun@gmail.com>
2024-04-30 15:31:21 -0700

    Add cross validation script

---
+++ src/cross_validate.py
@@ -0,0 +1,27 @@
+import hydra
+import rootutils
+from omegaconf import DictConfig
+
+rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+from utils import (
+    RankedLogger,
+    Task,
+    execute_task,
+    extras,
+    register_resolvers,
+)
+
+register_resolvers()
+
+log = RankedLogger(__name__, rank_zero_only=True)
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> None:
+    extras(cfg)
+    execute_task(Task.CrossValidate, cfg)
+
+
+if __name__ == "__main__":
+    main()
